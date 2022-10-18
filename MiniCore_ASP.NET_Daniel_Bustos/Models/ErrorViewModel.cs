811aa0d8dc06204b5a4aa8c10a5ce69295c17342
Daniel Bustos <daniel.bustos.velez@udla.edu.ec>
2022-10-17 23:15:09 -0500

    Creacion de proyecto

---
+++ MiniCore_ASP.NET_Daniel_Bustos/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace MiniCore_ASP.NET_Daniel_Bustos.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
